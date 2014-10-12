ade2b2e9eb653b1c91ebe7943f87cf9a06a86459
Valdior <valdior.tenshi@gmail.com>
2014-10-12 15:36:11 +0200

    Installation UserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,13 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            
+            // Bundle propre
             new CRM\CoreBundle\CRMCoreBundle(),
+            new CRM\UserBundle\CRMUserBundle(),
+            
+            // Bundle externe
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
